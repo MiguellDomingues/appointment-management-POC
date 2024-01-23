8fb60b7633f334b5b3ac95cb2da321df8489468e
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2024-01-23 14:24:04 -0800

    added Interval, IntervalSet classes to wrap calendar events

--- src/classes.js
+++ src/classes.js
@@ -1,5 +1,7 @@
-import { Interval } from './intervals.js'
+//import { Interval } from './intervals.js'
 import _ from 'lodash'
+
+const MAX_MINUTES_IN_DAY = 60*23 + 59
 
 const DOTW = Object.freeze({
     Monday: 0,
@@ -15,10 +17,7 @@ export class WorkDay{
 
     constructor(dotw, open, close, shifts, breaks){
 
-        if(!this.#isValidDay(dotw))
-            throw new Error( `dotw ${dotw} must be a day of the week or an integer 0-6`)
-        
-        this.dotw = _.isInteger(dotw) ? dotw : DOTW[dotw]
+        this.dotw = this.#parseDOTW(dotw)
         this.shifts = shifts || []
         this.breaks = breaks || []
 
@@ -29,9 +28,13 @@ export class WorkDay{
         }         
     }
 
-    #isValidDay(str){
-        return Object.keys(DOTW).includes(str) || //a string day
-               (str >= 0 && str <= 6)             // or an integer day 0-6
+    #parseDOTW(dotw){
+        if (_.isInteger(dotw) && (dotw >= 0 && dotw <= 6) ) 
+            return dotw
+        if (_.isString(dotw) && Object.keys(DOTW).includes(dotw)) 
+            return DOTW[dotw]
+        else 
+            throw new Error( `dotw ${dotw} must be a day of the week string or an integer 0-6`)         
     }
 
     isOpen(){
@@ -51,11 +54,87 @@ export class WorkDay{
     }
 }
 
+export class Interval {
+
+    constructor(start, end){
+  
+      if(_.isString(start))
+        this.start = this.hourMinutesStringtoTotalMinutes(start);
+      else 
+        this.start = start || 0
+      
+      if(_.isString(end))
+        this.end =   this.hourMinutesStringtoTotalMinutes(end);
+      else 
+        this.end = end || 0
+
+        if(this.end < 0 || this.start < 0 )
+            throw new Error(`start ${this.start} end ${this.end } must be > 0`)
+        if(this.start > this.end)
+            throw new Error(`start ${this.start} must be < end ${this.end }`)
+        if(this.end > MAX_MINUTES_IN_DAY)
+            throw new Error( `end ${this.end} must be <= ${MAX_MINUTES_IN_DAY}`)
+      
+      //assert.equal(this.end >= 0 && this.start >= 0 , true, `start ${this.start} end ${this.end } must be > 0`)
+     // assert.equal(this.start <= this.end, true, `start ${this.start} must be < end ${this.end }`)
+     // assert.equal(this.end <= MAX_MINUTES_IN_DAY , true, `end ${this.end} must be <= ${MAX_MINUTES_IN_DAY}`)
+    }
+  
+    get duration(){
+      return this.end-this.start;
+    }
+  
+    toObject(){
+      return {start: this.start, end: this.end}
+    }
+
+    hourMinutesStringtoTotalMinutes(timeString){
+        const regex = new RegExp(`^([0-1]?[0-9]|2[0-3]):([0-5][0-9])(:[0-5][0-9])?$`);
+
+        if(!regex.test(timeString)){
+            throw new Error(`time string ${timeString} must be 00:00->23:59 format`)
+        }
+    
+        return parseInt(timeString.split(":")[0])*60 + parseInt(timeString.split(":")[1])
+    } 
+  
+    startToHoursMinutesString(){
+      return Interval.totalMinutesToHoursMinutesString(this.start)
+    }
+  
+    endToHoursMinutesString(){
+      return Interval.totalMinutesToHoursMinutesString(this.end)
+    }
+
+    startTimeToHMObject(){
+        return Interval.totalMinutesToHoursMinutesObject(this.start)
+    }
+
+    endTimeToHMObject(){
+        return Interval.totalMinutesToHoursMinutesObject(this.end)
+    }
+
+    static totalMinutesToHoursMinutesObject(totalMins){
+        return ({h: Math.floor(totalMins/60) , m: totalMins%60})
+      }
+  
+    static totalMinutesToHoursMinutesString(totalMins){
+      return `${new String(Math.floor(totalMins/60)).padStart(2, '0')}:${new String(totalMins%60).padStart(2, '0') }`
+    }
+  
+    toString(){
+      return `start: ${this.startToHoursMinutesString()} end: ${this.endToHoursMinutesString()}`
+    }
+}
+
 export class IntervalSet{
 
-    constructor(start, end, set){
+    //missing_elements = []
+
+    constructor(start, end, set, missing_elements){
         this.interval = new Interval(start, end)
         this.set = _.cloneDeep(set) || []
+        this.missing_elements = _.cloneDeep(missing_elements) || []
     }
 
     get start(){
@@ -64,6 +143,14 @@ export class IntervalSet{
 
     get end(){
         return this.interval.end
+    }
+
+    startTimeToHMObject(){
+        return Interval.totalMinutesToHoursMinutesObject(this.interval.start)
+    }
+
+    endTimeToHMObject(){
+        return Interval.totalMinutesToHoursMinutesObject(this.interval.end)
     }
 
     toString(){
